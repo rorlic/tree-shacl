1a9c5faf4eb5b01c40dd21d7d02ebde565229907
Ranko Orlic <ranko@aquila-solutions.com>
2025-05-20 16:20:13 +0200

    chore: simplify tree:Relation sub-class validation

--- tests/tppr-13.test.ts
+++ tests/tppr-13.test.ts
@@ -5,11 +5,10 @@ import {
 
 describe('Test TPPR-13 (tree:path usage)', async () => {
   const validator = await createValidator(['tree-common-shapes.ttl']);
-  const pathRequiredShape = 'https://w3id.org/tree#RelationPathRequiredShape';
-  const treePath = 'https://w3id.org/tree#path';
+  const pathRequiredShape = 'https://w3id.org/tree#RelationSubclassPathRequiredShape';
 
   it('must contain a path predicate', async () => {
-    expectViolation({ sourceShape: pathRequiredShape, path: treePath } as ExpectedResult,
+    expectViolation({ sourceShape: pathRequiredShape } as ExpectedResult,
       await validateFile('./tests/tppr-13/missing-tree-path.ttl', validator), 10);
   });
 
--- tests/tppr-14.test.ts
+++ tests/tppr-14.test.ts
@@ -5,22 +5,20 @@ import {
 
 describe('Test TPPR-14 (tree:value usage)', async () => {
   const validator = await createValidator(['tree-common-shapes.ttl']);
-  const valueRequiredShape = 'https://w3id.org/tree#RelationValueRequiredShape';
-  const valueValidShape = 'https://w3id.org/tree#RelationValueKindShape';
-  const treePath = 'https://w3id.org/tree#value';
+  const valueRequiredShape = 'https://w3id.org/tree#RelationSubclassValueRequiredShape';
 
   it('must contain a value predicate', async () => {
-    expectViolation({ sourceShape: valueRequiredShape, path: treePath } as ExpectedResult,
+    expectViolation({ sourceShape: valueRequiredShape} as ExpectedResult,
       await validateFile('./tests/tppr-14/missing-tree-value.ttl', validator), 10);
   });
 
   it('must not have anything but a literal or URI as tree:value', async () => {
-    expectViolation({ sourceShape: valueValidShape, path: treePath } as ExpectedResult,
+    expectViolation({ sourceShape: valueRequiredShape } as ExpectedResult,
       await validateFile('./tests/tppr-14/invalid-tree-value.ttl', validator), 10);
   });
 
   it('must have a literal or URI as tree:value', async () => {
-    expectNoViolation(valueValidShape,
+    expectNoViolation(valueRequiredShape,
       await validateFile('./tests/tppr-14/valid-tree-value.ttl', validator));
   });
 });
