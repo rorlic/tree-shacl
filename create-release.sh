1725c27d429c6a9c896ffdd5bb7d936d0dc6ef98
Ranko Orlic <ranko@aquila-solutions.com>
2025-06-12 13:44:07 +0200

    chore: create both individual and combined shapes package

--- create-release.sh
+++ create-release.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
-rm -rf ./temp
 mkdir -p ./temp
+mkdir -p ./dist
+
+# create individual shapes package
+cd ./src
+zip ../dist/tree-src-shapes.zip *.ttl
+cd ..
 
 # create combined shapes files
+rm -rf ./temp/*
 cd ./src
 riot --quiet --formatted turtle $(tr '\n' ' ' < shapes-common.txt) > ../temp/tree-common-shapes.ttl
 riot --quiet --formatted turtle $(tr '\n' ' ' < shapes-root-node.txt) > ../temp/tree-root-node-shapes.ttl
 riot --quiet --formatted turtle $(tr '\n' ' ' < shapes-structure.txt) > ../temp/tree-structure-shapes.ttl
-cd ..
-
-# create package
-mkdir -p ./dist
-cd temp
+cd ../temp
 zip ../dist/tree-shapes.zip *
 cd ..
