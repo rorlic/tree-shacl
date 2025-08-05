abdcf91c0e2fde7c8df4da48dbbe2a12ad91bbf5
Ranko Orlic <ranko@aquila-solutions.com>
2025-08-05 16:14:58 +0200

    chore: rework WKT literal

--- tests/TR6.11.test.ts
+++ tests/TR6.11.test.ts
@@ -3,18 +3,18 @@ import {
   expectViolation, ExpectedResult, validateFile, createValidator, expectNoViolation,
 } from './utilities';
 
-describe('Test TPPR-24 (geospatially contains)', async () => {
-  const validator = await createValidator(['tree-common.ttl', 'tree-TPPR-24-geospatially-contains.ttl']);
+describe('Test TR6.11 (geospatially contains)', async () => {
+  const validator = await createValidator(['tree-common.ttl', 'TR6.11-geospatially-contains.ttl']);
   const valueValidShape = 'https://w3id.org/tree#GeospatiallyContainsValueKindShape';
   const treePath = 'https://w3id.org/tree#value';
 
   it('must not have anything but a WKT literal as tree:value', async () => {
     expectViolation({ sourceShape: valueValidShape, path: treePath } as ExpectedResult,
-      await validateFile('./tests/tppr-24/invalid-tree-value.ttl', validator), 4);
+      await validateFile('./tests/TR6.11/invalid-tree-value.ttl', validator), 4);
   });
 
   it('must have a WKT literal as tree:value', async () => {
     expectNoViolation(valueValidShape,
-      await validateFile('./tests/tppr-24/valid-tree-value.ttl', validator));
+      await validateFile('./tests/TR6.11/valid-tree-value.ttl', validator));
   });
 });
