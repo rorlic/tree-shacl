fade8b1d9aeafa2c9ab3c6749f1202e094a55917
Ranko Orlic <ranko@aquila-solutions.com>
2025-08-05 16:17:46 +0200

    chore: rework tree:path usage

--- tests/TR6.2.test.ts
+++ tests/TR6.2.test.ts
@@ -3,13 +3,13 @@ import {
   expectViolation, ExpectedResult, validateFile, createValidator,
 } from './utilities';
 
-describe('Test TPPR-13 (tree:path usage)', async () => {
-  const validator = await createValidator(['tree-common.ttl', 'tree-TPPR-13-tree-path-usage.ttl']);
+describe('Test TR6.2 (tree:path usage)', async () => {
+  const validator = await createValidator(['tree-common.ttl', 'TR6.2-tree-path-usage.ttl']);
   const pathRequiredShape = 'https://w3id.org/tree#RelationSubclassPathRequiredShape';
 
   it('must contain a path predicate', async () => {
     expectViolation({ sourceShape: pathRequiredShape } as ExpectedResult,
-      await validateFile('./tests/tppr-13/missing-tree-path.ttl', validator), 10);
+      await validateFile('./tests/TR6.2/missing-tree-path.ttl', validator), 10);
   });
 
   // TODO: validate that tree:path is a property path (see valid-tree-path.ttl) - use SHACL shapes SHACL if available
