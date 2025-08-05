b0ad2dce1c638070f294a9752f67fe8624bec932
Ranko Orlic <ranko@aquila-solutions.com>
2025-08-05 16:10:39 +0200

    chore: rework tree:shape constraints

--- tests/TR8.1.test.ts
+++ tests/TR8.1.test.ts
@@ -4,8 +4,8 @@ import {
   expectNoInfo, createValidator,
 } from './utilities';
 
-describe('Test TPCR-03 (tree:shape usage)', async () => {
-  const validator = await createValidator(['tree-common.ttl', 'tree-TPCR-03-tree-shape-usage.ttl']);
+describe('Test TR8.1 (tree:shape usage)', async () => {
+  const validator = await createValidator(['tree-common.ttl', 'TR8.1-tree-shape-usage.ttl']);
   const suggestedShape = 'https://w3id.org/tree#CollectionShapeSuggestedShape';
   const limitedShape = 'https://w3id.org/tree#CollectionShapeLimitedShape';
   const nodeKindShape = 'https://w3id.org/tree#CollectionShapeNodeKindShape';
@@ -14,28 +14,28 @@ describe('Test TPCR-03 (tree:shape usage)', async () => {
 
   it('may have zero tree:shape predicates', async () => {
     expectInfo({ sourceShape: suggestedShape, path: treeShape, focusNode: exampleCollection } as ExpectedResult, 
-      await validateFile('./tests/tpcr-03/no-tree-shape.ttl', validator));
+      await validateFile('./tests/TR8.1/no-tree-shape.ttl', validator));
   });
 
   it('must not have multiple tree:shape predicates', async () => {
     expectViolation({ sourceShape: limitedShape, path: treeShape, focusNode: exampleCollection } as ExpectedResult, 
-      await validateFile('./tests/tpcr-03/multiple-tree-shapes.ttl', validator));
+      await validateFile('./tests/TR8.1/multiple-tree-shapes.ttl', validator));
   });
 
   it('may have a single tree:shape predicate', async () => {
-    const report = await validateFile('./tests/tpcr-03/single-tree-shape.ttl', validator)
+    const report = await validateFile('./tests/TR8.1/single-tree-shape.ttl', validator)
     expectNoInfo(suggestedShape, report);
     expectNoViolation(limitedShape, report);
   });
 
   it('must not have invalid node shapes', async () => {
     expectViolation({ sourceShape: nodeKindShape, path: treeShape, focusNode: exampleCollection } as ExpectedResult, 
-      await validateFile('./tests/tpcr-03/invalid-tree-shapes.ttl', validator));
+      await validateFile('./tests/TR8.1/invalid-tree-shapes.ttl', validator));
   });
 
   it('may refer using its tree:shape property to one node shape', async () => {
     expectNoViolation(nodeKindShape,
-      await validateFile('./tests/tpcr-03/valid-tree-shapes.ttl', validator));
+      await validateFile('./tests/TR8.1/valid-tree-shapes.ttl', validator));
   });
 
 });
