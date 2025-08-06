2ac59c750b65c1b6dd7728a49e6fc69a29f0d169
Ranko Orlic <ranko@aquila-solutions.com>
2025-08-06 10:38:28 +0200

    fix: no duplicate prefixes

--- build/create-release.sh
+++ build/create-release.sh
@@ -11,14 +11,9 @@ cd ..
 # create combined shapes files
 rm -rf ./temp/*
 cd ./src
-riot --quiet --formatted turtle $(tr '\n' ' ' < ../build/shapes-common.txt) > ../temp/tree-collection-node-shapes.ttl
-riot --quiet --formatted turtle $(tr '\n' ' ' < ../build/shapes-common.txt) > ../temp/tree-root-node-shapes.ttl
-riot --quiet --formatted turtle $(tr '\n' ' ' < ../build/shapes-common.txt) > ../temp/tree-subsequent-node-shapes.ttl
-
-riot --quiet --formatted turtle $(tr '\n' ' ' < ../build/shapes-node-collection.txt) >> ../temp/tree-collection-node-shapes.ttl
-riot --quiet --formatted turtle $(tr '\n' ' ' < ../build/shapes-node-root.txt) >> ../temp/tree-root-node-shapes.ttl
-riot --quiet --formatted turtle $(tr '\n' ' ' < ../build/shapes-node-subsequent.txt) >> ../temp/tree-subsequent-node-shapes.ttl
-
+riot --quiet --formatted turtle $(tr '\n' ' ' < ../build/shapes-common.txt) $(tr '\n' ' ' < ../build/shapes-node-collection.txt) > ../temp/tree-collection-node-shapes.ttl
+riot --quiet --formatted turtle $(tr '\n' ' ' < ../build/shapes-common.txt) $(tr '\n' ' ' < ../build/shapes-node-root.txt) > ../temp/tree-root-node-shapes.ttl
+riot --quiet --formatted turtle $(tr '\n' ' ' < ../build/shapes-common.txt) $(tr '\n' ' ' < ../build/shapes-node-subsequent.txt) > ../temp/tree-subsequent-node-shapes.ttl
 riot --quiet --formatted turtle $(tr '\n' ' ' < ../build/shapes-structure.txt) > ../temp/tree-structure-shapes.ttl
 cd ../temp
 rm -f ../dist/tree-shapes.zip
